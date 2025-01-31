ab17ede6944c7023eef983e79f6fb2fefa9e0cd6
Diego-Silva-02 <diegosilva22a@gmail.com>
2025-01-31 16:42:48 -0300

    Add project files.

---
+++ SqsPublisher/CostumerCreated.cs
@@ -0,0 +1,11 @@
+﻿namespace SqsPublisher
+{
+    internal class CostumerCreated
+    {
+        public required Guid Id { get; set; }
+        public required string FullName { get; set; }
+        public required string Email { get; set; }
+        public required string GitHubUserName { get; set; }
+        public required DateTime DateOfBirth { get; set; }
+    }
+}
---
+++ SqsPublisher/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using SqsPublisher;
+
+var sqsClient = new AmazonSQSClient();
+
+var customer = new CostumerCreated
+{
+    Id = Guid.NewGuid(),
+    Email = "diegosilva22a@gmail.com",
+    FullName = "Diego Silva",
+    DateOfBirth = new DateTime(2001, 8, 28),
+    GitHubUserName = "Diego-Silva-02"
+};
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+
+var sendMessageRequest = new SendMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(customer),
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>
+    {
+        {
+            "MessgeType", new MessageAttributeValue
+            {
+                DataType = "String",
+                StringValue = nameof(CostumerCreated)
+            }
+        }
+    }
+};
+
+var response  = await sqsClient.SendMessageAsync(sendMessageRequest);
+
+Console.WriteLine();
